33156fe4c51554e1141496e1ea08ed376f84389e
Thomi Jasir <thomi.jasir@dana.id>
2022-04-18 15:06:03 +0700

    docs(Readme): add readme and update detail pages

---
+++ readme.md
@@ -0,0 +1,34 @@
+# Country Search - Rect Hook 18
+------------------
+
+Hello every one, this is one of my sample test project, using hook and react 18, i build this project using my own architecture start from scratch.
+
+Setting webpack and configuration by myself so its more scalability because, i maintenance all of this react script instead of CRA + eject difficult to maintain and many vulnerability that difficult to solve.
+
+and the other thing you might be know this project is zero vulnerability because all of script and configuration on this project i made by my self, and also i maintain all package that need to install in this project and remove all unnecessary package.
+
+## Feature Application
+
+1. AutoComplete Search List
+2. Custom Hook API
+3. Arrow up and down and enter support
+4. Custom App Context (Inspire Redux)
+5. Full Restrict TypeScript
+6. New Architecture React Hook Template
+7. Call API after user finish typing
+8. New API Interface
+9. Fast webpack build
+10. Customize webpack bundle and build
+
+### Wanna try in you local machine?
+
+just pull this project run the react
+```
+- npm install
+- npm run dev:stag
+```
+
+### Demo App
+------------------
+don't want running project on local? here as the sample output.
+![Demo Application](https://cdn.glitch.com/worldscollide2.jpg)
--- src/container/DetailCountry/DetailCountry.page.tsx
+++ src/container/DetailCountry/DetailCountry.page.tsx
@@ -35,7 +35,43 @@ const DetailCountry: FC<IProps> = () => {
 
   return (
     <div className="detail-country-page safe-area">
-      {JSON.stringify(country)}
+      <div className="flag">
+        <img src={country?.flags.svg} alt="Flag of country" />
+      </div>
+      <div className="country-info">
+        <div className="item">
+          <div className="col-left">Name</div>
+          <div className="col-right">
+            {country?.name} - {country?.nativeName}
+          </div>
+        </div>
+        <div className="item">
+          <div className="col-left">Population</div>
+          <div className="col-right">
+            {new Intl.NumberFormat().format(
+              parseInt(country?.population.toString() || '0', 10),
+            )}
+          </div>
+        </div>
+        <div className="item">
+          <div className="col-left">Capital</div>
+          <div className="col-right">{country?.capital}</div>
+        </div>
+        <div className="item">
+          <div className="col-left">Currency</div>
+          <div className="col-right">
+            {country?.currencies[0].code} - {country?.currencies[0].symbol}
+          </div>
+        </div>
+        <div className="item">
+          <div className="col-left">Regional</div>
+          <div className="col-right">{country?.region}</div>
+        </div>
+        <div className="item">
+          <div className="col-left">Sub Region</div>
+          <div className="col-right">{country?.subregion}</div>
+        </div>
+      </div>
     </div>
   );
 };
